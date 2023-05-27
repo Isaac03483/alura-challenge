47a2a372a0f4ae46c8dc1639f151b5e6ec163fd9
Levi <levihernandez2501@gmail.com>
2023-05-26 19:54:32 -0600

    Agregando logica a los botones

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function encriptar(){
-    const value = document.getElementById("input-text").value;
+    let value = document.getElementById("input-text").value;
     console.log(value);
     if(value == ''){
         document.getElementById("empty-div").style.display = "block";
@@ -9,11 +9,19 @@ function encriptar(){
 
     document.getElementById("empty-div").style.display = "none";
     document.getElementById("non-empty-div").style.display = "block";
+
+    value = value.replaceAll('e','enter');
+    value = value.replaceAll('i','imes');
+    value = value.replaceAll('a','ai');
+    value = value.replaceAll('o','ober');
+    value = value.replaceAll('u','ufat');
+
+    document.getElementById("output-text").value = value;
 
 }
 
 function desencriptar(){
-    const value = document.getElementById("input-text").value;
+    let value = document.getElementById("input-text").value;
     console.log(value);
     if(value == ''){
         document.getElementById("empty-div").style.display = "block";
@@ -23,4 +31,22 @@ function desencriptar(){
 
     document.getElementById("empty-div").style.display = "none";
     document.getElementById("non-empty-div").style.display = "block";
+
+    value = value.replaceAll('enter','e');
+    value = value.replaceAll('imes','i');
+    value = value.replaceAll('ai','a');
+    value = value.replaceAll('ober','o');
+    value = value.replaceAll('ufat','u');
+
+    document.getElementById("output-text").value = value;
+}
+
+function copy(){
+    const copyText = document.getElementById('output-text');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    alert("texto copiado");
 }
